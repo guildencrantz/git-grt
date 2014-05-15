fbb22a199ce1009bcb11f93b7f2a31c3b1e66529
Michael Miller <michael.miller@returnpath.com>
2014-05-15 13:24:42 -0600

    List calls now return results
    Change-Id: I054807c3dc72995d8ab1f9eebf31daced8804a37

--- grt-list.go
+++ grt-list.go
@@ -1,6 +1,27 @@
 package main
 
-import ()
+import (
+    "fmt"
+    "encoding/json"
+)
+
+type listChangeInfo struct {
+	Kind       string
+	Id         string
+	Project    string
+	Branch     string
+	Change_id  string
+	Subject    string
+	Status     string
+	Created    string
+	Updated    string
+	Mergeable  int
+	Sortkey   string `json:"_sortkey"`
+	Number    int    `json:"_number"`
+	Owner      struct {
+		Name string
+	}
+}
 
 func listCmd(args []string) int {
 	if len(args) <= 0 {
@@ -12,5 +33,12 @@ func listCmd(args []string) int {
 
 func listCmdDefault() {
 	cmd := NewGrtCmd("GET", "/a/changes/")
-	cmd.Call()
+	resp := cmd.Call()
+
+    var list []listChangeInfo
+    json.Unmarshal([]byte(resp), &list)
+
+    for i := 0; i < len(list); i++ {
+        fmt.Println(list[i].Sortkey)
+    }
 }
