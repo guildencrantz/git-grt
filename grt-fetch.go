232b397544a8965ca4f3c5f46596308d1a8a4448
Matt Henkel <matt.henkel@returnpath.com>
2014-05-16 13:54:02 -0600

    Working fetch command.
    Currently need to add the config value gerritnumber (the gerrit number
    from the URL in the web page, or _number from the ChangeInfo object) to
    the branch's config.
    
    Ex: If you are on branch "my_branch" and the url to your change is
    http://gerrit.dev.returnpath.net/#/c/27436/ then you need to add the
    config like:
    
    git config --add branch.my_branch.gerritnumber 27436
    
    Change-Id: Ideefecdbee422e7a6ddf4fd883ddac2ce2e323bd

--- grt-fetch.go
+++ grt-fetch.go
@@ -8,35 +8,47 @@ import (
 )
 
 func Fetch(args []string) {
-	// The end point is: project~branch~change_id
-	//endpoint := change_endpoint + "auth_policy_publisher~master~Ib0a47bffa7ddee956d980df3f799ebafb9ae1f2a"
-	endpoint := change_endpoint + "gerrit-sandbox~master~Ib4d99019ceaf282a9e4b20322d840099611ca1cb"
-	cmd := NewGrtCmd("GET", endpoint)
-	cmd.rawForm = "o=ALL_REVISIONS&o=DOWNLOAD_COMMANDS"
+	number := fmt.Sprintf("branch.%s.gerritnumber", GetCurrentBranch())
 
-	changeInfo := ChangeInfo{}
+	// The end point is: project~branch~change_id
+	cmd := NewGrtCmd("GET", change_endpoint)
+
+	// I'm using the query instead of the direct changeset endpoint because then
+	// we don't have to store the gerrit id (project~branch~change_id)
+	cmd.getVars = make(map[string]string)
+	cmd.getVars["q"] = GetConfigValue(number)
+	cmd.getVars["o"] = "ALL_REVISIONS&o=DOWNLOAD_COMMANDS"
+
 	resp := cmd.Call()
 
-	println(resp)
-
+	changeInfo := []ChangeInfo{}
 	json.Unmarshal([]byte(resp), &changeInfo)
 
-	// Currently the FetchInfo isn't being populated in 2.8.1, but we can calculate the info.
-	changeInfo.fetchRevisions()
+	// the info. We specify the gerrit changeset number so we should only get one
+	// response, but we're using the query interface so we have to pull that
+	// response out of a list.
+	changeInfo[0].fetchRevisions()
 }
 
 func (changeInfo ChangeInfo) fetchRevisions() {
 	for hash, revisionInfo := range changeInfo.Revisions {
 		_, err := execCommand("log", "-n 0", hash)
 		// The revision isn't being tracked if trying to get it's log
-		// returns an error.
+		// returns an error. If we already have the hash we assume it's tagged.
 		if err != nil {
+			// Currently the FetchInfo isn't being populated in 2.8.1, so we calculate it
 			// The root of the refspec is the last two digits of the change number.
 			number := strconv.Itoa(changeInfo.Number)
 			root := number[len(number)-2:] // Why no negative indexes golang?
-			ref := fmt.Sprintf("%s/%s/%s", root, number, strconv.Itoa(revisionInfo.Number))
+			rev := strconv.Itoa(revisionInfo.Number)
+			ref := fmt.Sprintf("refs/changes/%s/%s/%s", root, number, rev)
 
 			out, err := execGit("fetch", "gerrit", ref); if err != nil {
+				log.Fatal(out)
+			}
+
+			tag := fmt.Sprintf("%s_%s", number, rev)
+			out, err = execGit("tag", tag, hash); if err != nil {
 				log.Fatal(out)
 			}
 		}
