01ab74a4d9f8ec8d9feb763e7497df57a17f4d99
Matt Henkel <matt.henkel@returnpath.com>
2014-05-16 10:18:16 -0600

    Fleshing out changes structures.
    Extending ChangeInfo for more optional fields.
    Adding FetchInfo and RevisionInfo structs.
    
    Change-Id: I51732da8a32a7a2f9b402c7fcb91c79702983b9f

--- changes.go
+++ changes.go
@@ -9,27 +9,54 @@ const (
 )
 
 type ChangeInfo struct {
-	Kind       string
-	Id         string
-	Project    string
 	Branch     string
-	Topic      string
 	ChangeId   string `json:"change_id"`
-	Subject    string
-	Status     string
 	Created    string
-	Updated    string
-	Mergeable  int
-	Insertions int
+	CurrentRevision string `json:"current_revision"`
 	Deletions  int
-	Sortkey    string `json:"_sortkey"`
-	Number     int    `json:"_number"`
+	Id         string
+	Insertions int
+	Kind       string
+	Mergeable  int
+	Number     int `json:"_number"`
 	Owner      struct {
 		Name string
 	}
+	Project    string
+	Reviewed   string
+	Revisions  map[string]RevisionInfo
+	Sortkey    string `json:"_sortkey"`
+	Status     string
+	Subject    string
+	Topic      string
+	Updated    string
 }
 
 func (changeInfo ChangeInfo) String() string {
 	ret, _ := json.Marshal(changeInfo)
 	return string(ret)
 }
+
+type FetchInfo struct {
+	Url string
+	Ref string
+}
+
+func (fetchInfo FetchInfo) String() string {
+	ret, _ := json.Marshal(fetchInfo)
+	return string(ret)
+}
+
+type RevisionInfo struct {
+	Draft            string
+	HasDraftComments bool `json:"has_draft_comments"`
+	Number           int `json:"_number"`
+	Fetch            map[string]FetchInfo
+}
+
+func (revisionInfo RevisionInfo) String() string {
+	ret, _ := json.Marshal(revisionInfo)
+	return string(ret)
+}
+
+
--- grt-fetch.go
+++ grt-fetch.go
@@ -1,9 +1,21 @@
 package main
 
-func Fetch(args []string) {
-	// curl --verbose -u mhenkel:ZvGPregVQNkD --digest http://gerrit.dev.returnpath.net/a/changes/auth_policy_publisher\~master\~Ib0a47bffa7ddee956d980df3f799ebafb9ae1f2a\?o\=ALL_REVISIONS
+import (
+	"encoding/json"
+)
 
-	cmd := NewGrtCmd("GET", change_endpoint)
+func Fetch(args []string) {
+	// The end point is: project~branch~change_id
+	endpoint := change_endpoint + "auth_policy_publisher~master~Ib0a47bffa7ddee956d980df3f799ebafb9ae1f2a"
+	cmd := NewGrtCmd("GET", endpoint)
 	cmd.rawForm = "o=ALL_REVISIONS"
+
+	changeInfo := ChangeInfo{}
+	resp := cmd.Call()
+	println(resp)
+
+	json.Unmarshal([]byte(resp), &changeInfo)
+	println(changeInfo.String())
+
 }
 
